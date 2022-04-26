b41d8edf8867a09db69b6fa74d652bc0b42ba608
Michelle <clairsin@gmail.com>
2022-04-26 11:34:38 -0400

    commit tree 86651a40af2cfc6679851169190394aab50534e4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+let scuberHQ = 42
+let feetPerBlock = 264
+
+function distanceFromHqInBlocks(pickup) {
+  let result = pickup > scuberHQ ? pickup - scuberHQ : scuberHQ - pickup *1
+  return result
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup)*feetPerBlock
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(((start-end))*feetPerBlock)
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination)
+  if(distance <=400) {
+    return 0
+  } else if (distance >400 && distance <=2000) {
+    return (distance-400)*0.02
+  } else if (distance >2000 && distance <=2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
